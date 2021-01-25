d283a5860e332b464efd7da32e156e63a9d7c141
孤独な 宇宙飛行士 <vadim1230981@gmail.com>
2021-01-26 01:38:37 +0300

    Setting up lint, tsc & husky.

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Triggering sonarcloud
+//Triggering sonarcloud
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
-	res.json('Hello world');
-})
+    res.json('Hello world');
+});
 
 app.listen(port, () => console.log(`Server running at ${port}`));
